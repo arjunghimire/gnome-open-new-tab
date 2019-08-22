e5617251c5bb5ecd5062d1a4c814ad9b5a4dbeac
Arjun Ghimire <arjunghimire0714@gmail.com>
2019-08-22 13:25:59 +0545

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 gnome-terminal --tab -e 'bash -c "emulator -avd Nexus_5X_API_29_x86; exec bash"'
 gnome-terminal --tab -e 'bash -c "cd Projects/NewApp && react-native start; exec bash"'
 gnome-terminal --tab -e 'bash -c "cd Projects/NewApp && react-native run-android; exec bash"'
-gnome-terminal --tab -e 'bash -c "cd Projects/MyProjects/NewApp && code .; exec bash"'
+gnome-terminal --tab -e 'bash -c "cd Projects/NewApp && code .; exec bash"'
